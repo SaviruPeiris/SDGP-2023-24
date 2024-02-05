8b1769c207d6100a461476b1bab3ecc1cc1c3335
Saviru Peiris <124691442+SaviruPeiris@users.noreply.github.com>
2024-02-05 21:32:13 +0530

    Update footer.jsx

---
+++ footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer className='py-3 text-center text-white' style={{
+    backgroundColor:'#0A1172',
+    marginTop:'15px',
+    marginBottom:'25px',
+  }}>
+    <div className='container mx-auto '>
+      Copyight &copy; 2024. All rights Reserved.
+    </div>
+  </footer>;
+};
+
+export default Footer;
