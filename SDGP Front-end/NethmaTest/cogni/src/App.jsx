ab6c8889307dc03ac276bb90fa00ef535848b625
Nethma Jatunarachchi <nethmajanz@Nethmas-MacBook-Pro.local>
2024-02-12 17:23:00 +0530

    nethma cards

--- SDGP Front-end/NethmaTest/cogni/src/App.jsx
+++ SDGP Front-end/NethmaTest/cogni/src/App.jsx
@@ -1,46 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Home } from "./pages/home/Home"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { DetailsPages } from "./pages/details/DetailsPages"
 
+
+const App = () => {
   return (
     <>
-
-
-    
-      {
-        <div>
+      <Router>
+     
+        <Switch>
+          <Route exact path='/' component={Home} />
           
-        </div>
-      
-      /* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-
-
-
+          <Route exact path='/details/:id' component={DetailsPages} />
+          
+        </Switch>
+       
+      </Router>
     </>
   )
 }
-
 export default App
---
+++ SDGP Front-end/NethmaTest/cogni/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Card } from "../../components/blog/Card"
+
+
+export const Home = () => {
+  return (
+    <>
+      {/*  <Slider />*/}
+      <Card />
+    </>
+  )
+}
