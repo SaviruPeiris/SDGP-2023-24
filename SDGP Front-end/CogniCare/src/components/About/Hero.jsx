fa729c8cf7939977eac279e168a45f03b93fead9
Tenuri <tenuriw2002@gmail.com>
2024-03-15 16:39:39 +0530

    Link About page with latest updates

--- SDGP Front-end/CogniCare/src/components/About/Hero.jsx
+++ SDGP Front-end/CogniCare/src/components/About/Hero.jsx
@@ -1,29 +1,15 @@
-import React from "react";
-// import Dementia from '../assets/aboutImages/pic4.jpg';
-import Dementia from "../../assets/images/aboutPage/pic4.jpg";
+import React from 'react';
+import Dementia from '../assets/aboutImages/pic4.jpg';
 
 const Hero = () => {
   return (
-    <div className="w-full bg-purple-300 py-4 px-4">
-      <div className="max-w-[1160px] mx-auto grid md:grid-cols-2 gap-10">
-        <img
-          className="w-full md:w-[850px] mx-auto my-1"
-          src={Dementia}
-          alt="Laptop"
-        />
-        <div className="flex flex-col justify-center">
-          <h1 className="md:text-3xl sm:text-2xl text-xl font-bold py-2 mb-6 text-white">
-            OUR VISION
-          </h1>
-          <p className="text-[#4a236f] text-lg">
-            Create a world where individuals and families affected by dementia
-            find solace, support, and empowerment. We strive to be a beacon of
-            understanding, providing innovative solutions that enhance the
-            quality of life for those navigating the challenges of dementia.
-            Through empathy, education, and technology, we envision a future
-            where the journey with dementia is met with compassion, dignity, and
-            a sense of community.
-          </p>
+    <div className='bg-purple-300 py-4 px-4'>
+      <div className='max-w-[1160px] mx-auto grid md:grid-cols-2 gap-10 items-center justify-center text-center'>
+        <img className='w-full md:w-[850px] h-auto mx-auto my-1' src={Dementia} alt='Laptop' />
+        <div className='text-white'>
+          <h1 className='md:text-5xl sm:text-2xl text-xl font-bold py-2 mb-2'>OUR VISION</h1>
+          <p className="text-xl mb-4 text-purple-800">Welcome to our Dementia Awareness Platform!</p>
+          <p className="text-purple-700 text-3xl font-semibold mb-4">Our vision is to utilize cutting-edge technology and compassionate care to predict and support those affected by dementia.</p>
         </div>
       </div>
     </div>
