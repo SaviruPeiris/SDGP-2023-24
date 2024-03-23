2d2cd89976ad372617eed3072d44a8338945537b
Rashini Nawanga <rashininawanga20@gmail.com>
2024-03-23 12:25:19 +0530

    update index.js file

--- Authentication-Backend/src/routes/v1/index.js
+++ Authentication-Backend/src/routes/v1/index.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const userRoutes = require('./user.routes')
-const authRoutes = require('./auth.routes')
+const userRoutes = require('./user.routes');
+const authRoutes = require('./auth.routes');
+const appointmentRoutes = require('./appoinments.routes'); 
 
-router.use('/users', userRoutes)
-router.use('/auth' , authRoutes)
-module.exports = router
+router.use('/users', userRoutes);
+router.use('/auth', authRoutes);
+router.use('/appointments', appointmentRoutes); 
+
+module.exports = router;
