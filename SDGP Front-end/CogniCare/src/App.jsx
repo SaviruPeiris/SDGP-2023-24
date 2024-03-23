e80317a78b6ae6f1063beea63a16399389f36b8b
Rashini Nawanga <rashininawanga20@gmail.com>
2024-03-23 15:12:25 +0530

    Added feature to remember user ID using localStorage

--- SDGP Front-end/CogniCare/src/App.jsx
+++ SDGP Front-end/CogniCare/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
+import {useEffect, useState} from "react";
+//import viteLogo from "/vite.svg";
 import Navbar from "./components/NavBar";
 import LeftContent from "./components/LeftContent";
 import CardContainer from "./components/CardContainer";
@@ -9,69 +9,71 @@ import NavBarTest from "./components/NavBar/NavBarTest";
 import "./App.css";
 import Home from "./pages/Home";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate, useNavigate} from "react-router-dom";
 import MemoryGame from "./pages/MemoryGame";
 import PredictionPage from "./pages/prediction/PredictionPage";
 import AboutPage from "./pages/aboutpage/aboutPage";
 import ContactUsPage from "./pages/contactUsPage/ContactUs";
 import AwarenessPage from "./pages/AwaranessPage/awareness";
 import AwarenessHomePage from "./pages/AwaranessPage/Home";
-import ImageUploadAwareness from "./pages/ImageUploadAwareness/ImageUploadAwareness";
-import RegisterPage from "./pages/Loginpage/Register";
+import ImageUploadAwareness from "./pages/ImageUploadAwareness/ImageUploadAwareness"
+import RegisterPage from "./pages/Loginpage/Register";                                 
 import LoginPage from "./pages/Loginpage/Login";
-import UserProfile from "./pages/UserProfile/UserProfile";
+import UserProfile from "./pages/UserProfile/UserProfile"
 import DetailPage from "./pages/DetailsPage/DetailPage";
 import EChannelingPage from "./pages/E-ChannelingPage/EChannelingPage";
 // import Card from "./pages/AwaranessPage/Card";
-import ScrollToTopOnPageChange from "./components/ScrollToTopOnPageChange";
 
 function App() {
+
+    const [userId, setUserId] = useState(localStorage.getItem("userId"));
+
+    useEffect(() => {
+        const storedUserId = localStorage.getItem("userId");
+        if (!storedUserId) {
+            // If no user ID in local storage, set userId state to null
+            setUserId(null);
+        } else {
+            // If user ID exists in local storage, set userId state
+            setUserId(storedUserId);
+        }
+    }, []);
+
+    // const {PUBLIC_URL} = process.env
+
   return (
     // <>
     //   <NavBarTest></NavBarTest>
     //   <LeftContent></LeftContent>
     //   <CardContainer></CardContainer>
     // </>
-    
-    <BrowserRouter>
-      <ScrollToTopOnPageChange />
-      <Routes>
-        <Route index element={<Home></Home>} />
-        <Route path="/home" element={<Home></Home>} />
-        <Route path="/memorygame" element={<MemoryGame></MemoryGame>} />
-        <Route
-          path="/predictionPage"
-          element={<PredictionPage></PredictionPage>}
-        />
-        <Route
-          path="/imageUploadAwareness"
-          element={<ImageUploadAwareness></ImageUploadAwareness>}
-        />
-        <Route path="/registerPage" element={<RegisterPage></RegisterPage>} />
-        <Route path="/loginPage" element={<LoginPage></LoginPage>} />
-       
-        <Route
-          path="/predictionPage"
-          element={<PredictionPage></PredictionPage>}
-        />
-        <Route path="/about" element={<AboutPage></AboutPage>} />
-        <Route
-          path="/contactUsPage"
-          element={<ContactUsPage></ContactUsPage>}
-        />
-        <Route
-          path="/awarenessPage"
-          element={<AwarenessHomePage></AwarenessHomePage>}
-        />
-        <Route
-          path="/eChannelingPage"
-          element={<EChannelingPage></EChannelingPage>}
-        />
-        <Route path="/myProfilePage" element={<UserProfile></UserProfile>} />
-        <Route path="/details/:id" element={<DetailPage></DetailPage>} />
-        
-        {/* <Route exact path="/card" component={<Card></Card>} /> */}
-      </Routes>
+
+    <BrowserRouter >
+        <Routes>
+            {/* Routes accessible regardless of authentication */}
+            <Route path="/registerPage" element={<RegisterPage />} />
+            <Route path="/loginPage" element={<LoginPage />} />
+
+            {/* Routes accessible only when userId is available */}
+            {userId && (
+                <>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/memorygame" element={<MemoryGame />} />
+                    <Route path="/predictionPage" element={<PredictionPage />} />
+                    <Route path="/imageUploadAwareness" element={<ImageUploadAwareness />} />
+                    <Route path="/predictionPage" element={<PredictionPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/contactUsPage" element={<ContactUsPage />} />
+                    <Route path="/awarenessPage" element={<AwarenessHomePage />} />
+                    <Route path="/eChannelingPage" element={<EChannelingPage />} />
+                    <Route path="/myProfilePage" element={<UserProfile />} />
+                    <Route path="/details/:id" element={<DetailPage />} />
+                </>
+            )}
+            {/* Redirect to login page if userId is not available */}
+            {!userId && <Route path="*" element={<Navigate to="/loginPage" />} />}
+        </Routes>
+
     </BrowserRouter>
   );
 }
