2a27e42493b8de174404f99bf2fe644b307579ed
Rashini Nawanga <rashininawanga20@gmail.com>
2024-03-23 12:11:48 +0530

    Update user.routes.js

--- Authentication-Backend/src/routes/v1/user.routes.js
+++ Authentication-Backend/src/routes/v1/user.routes.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user.controller')
-
+const upload = require('../../middleware/filestore.middleware');
 
 router.post('/',  userController.createUser)
 router.get('/',  userController.getUsers)
-router.get('/logged-in-user/', userController.getUserById)
+router.get('/:id', userController.getUserById)
+router.put('/:id' , userController.editUser)
+router.put('/file/:id' , userController.uploadFile)
+
 
 module.exports = router;
