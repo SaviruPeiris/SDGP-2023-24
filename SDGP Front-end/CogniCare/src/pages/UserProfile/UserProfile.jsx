9b03f6ee364589d4e9b69c24205969e7ae10a632
Nethma Jatunarachchi <nethmajanuki@gmail.com>
2024-03-24 13:02:58 +0530

    Update the nav bar in UserProfile

--- SDGP Front-end/CogniCare/src/pages/UserProfile/UserProfile.jsx
+++ SDGP Front-end/CogniCare/src/pages/UserProfile/UserProfile.jsx
@@ -1,14 +1,11 @@
-// import './App.css';
-import Footer from "../../components/Footer";
-import NavBarTest from "../../components/NavBar/NavBarTest";
 import UserProfile from "../../components/UserProfile/UserProfile";
 
 function App() {
   return (
     <div>
-      <NavBarTest></NavBarTest>
+     
       <UserProfile />
-      <Footer></Footer>
+      
     </div>
   );
 }
